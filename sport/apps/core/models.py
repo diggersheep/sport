468b8c23a42c684b0063479492b906876992ba3c
a.combeau <a.combeau@unistra.fr>
2019-12-18 16:49:52 +0100

    d

--- sport/apps/core/models.py
+++ sport/apps/core/models.py
@@ -2,7 +2,7 @@ from typing import List
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models import Q
+from django.db.models import Q, Max
 
 
 # Create your models here.
@@ -11,6 +11,7 @@ class Exercice(models.Model):
     name = models.CharField(max_length=32, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     common = models.BooleanField(default=False, null=False)
+    counter = models.BooleanField(default=False, null=False)
 
     @classmethod
     def get_my_exercices(cls, user: User) -> List[models.Model]:
@@ -23,6 +24,24 @@ class Serie(models.Model):
     """Exervice class"""
     exercice = models.ForeignKey(Exercice, on_delete=models.CASCADE, null=False)
     weight = models.FloatField(default=0.0)
+    reps = models.IntegerField(default=0, null=False)
     date = models.DateTimeField(auto_now=True, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    counter = models.BooleanField(default=False, null=False)
+
+    @classmethod
+    def get_home_series(cls, exos: List[Exercice]):
+        results = []
+        for exo in exos:
+            objs = Serie.objects.filter(exercice_id=exo)
+            if exo.counter:
+                objs = objs.aggregate(Max('reps'))
+            else:
+                objs = objs.aggregate(Max('weight'))
+
+            print(len(objs))
+            if len(objs) > 0:
+                results.append(objs)
+                print(objs)
+
+
+        return results
--- sport/apps/core/views.py
+++ sport/apps/core/views.py
@@ -1,7 +1,27 @@
 
 # Create your views here.
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def home(request):
-    return render(request, 'base.html')
+class HomeView(TemplateView):
+    template_name = 'home.html'
+    redirect = '/login'
+
+    def get(self, request, **kwargs):
+        if request.user and request.user.is_authenticated:
+            return self.home(request, **kwargs)
+        else:
+            return HttpResponseRedirect(self.redirect)
+
+    def home(self, request, **kwargs):
+        from sport.apps.core.models import Exercice, Serie
+
+
+        exos = Exercice.get_my_exercices(request.user)
+        measures = Serie.get_home_series(exos)
+        context = {
+            'exos': exos
+        }
+        return render(request, self.template_name, context=context)
