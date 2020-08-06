b66a2610ed25e9821de23ec1686832003c88cb73
diggersheep <alexandre.combeau@outlook.fr>
2020-08-07 01:19:02 +0200

    refactor views

--- sport/urls.py
+++ sport/urls.py
@@ -3,26 +3,24 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from sport.apps.core import views
-from sport.apps.core.views import ErrorView, SeriesView, \
-    post_serie_delete, post_add_serie, get_additionnal_info, theme_view
-from sport.apps.login.views import LoginView, LogoutView
+from sport.apps.login.views import (LoginView, LogoutView)
 from sport.apps.stats.views import StatsView
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('error/<int:code>', ErrorView.as_view(), name="error"),
+    path('error/<int:code>', views.get_error, name="error"),
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('admin/', admin.site.urls),
 
     path('machine/<int:machine_id>', views.machine, name='machine'),
-    path('exercise', post_serie_delete, name='del_serie'),
+    path('exercise', views.post_serie_delete, name='del_serie'),
     path('exercise/<int:exo_id>', views.exercise, name='exercise'),
-    path('exercise/add_serie/', post_add_serie, name='add_serie'),
+    path('exercise/add_serie/', views.post_add_serie, name='add_serie'),
 
     path('stats/', StatsView.as_view(), name='stats'),
-    path('series/', SeriesView.as_view(), name='series'),
+    path('series/', views.get_series, name='series'),
 
-    path('info/<str:page>', get_additionnal_info, name='info'),
-    path('theme/<str:theme_name>', theme_view, name='theme'),
+    path('info/<str:page>', views.get_additional_info, name='info'),
+    path('theme/<str:theme_name>', views.theme_view, name='theme'),
 ]
